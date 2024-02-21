a3e2f69ba605a6350a97bd3e7f7826a0998a5754
Ramesh <rameshredddy.salla@gmail.com>
2024-02-21 14:27:45 +0530

    updated changes

--- src/main/java/com/spring/io/configserver/ConfigServerApplication.java
+++ src/main/java/com/spring/io/configserver/ConfigServerApplication.java
@@ -3,8 +3,10 @@ package com.spring.io.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 @EnableConfigServer
 public class ConfigServerApplication {
 
